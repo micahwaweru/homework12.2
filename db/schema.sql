86652c3e34a4f9724c5ffb591a62423f28284f82
micahwaweru <micah.waweru@gmail.com>
2021-11-17 19:12:12 -0600

    build schema, start seed but need to test schema first

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,30 @@
-DROP DATABASE IF EXISTS registrar_db;
-CREATE DATABASE registrar_db;
+DROP DATABASE IF EXISTS staff_db;
+CREATE DATABASE staff_db;
+
+DROP TABLE IF EXISTS department;
+
+
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR (30),
+  role_id INTEGER (10),
+  manager_id INTEGER (10) NULL,
+  PRIMARY KEY (id)
+);
