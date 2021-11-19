d09822e9ff00188fa4d3a9ede6d6d67551ceff2a
micahwaweru <micah.waweru@gmail.com>
2021-11-19 14:02:18 -0600

    add list and switch case, connect independent functions to switch case

--- index.js
+++ index.js
@@ -3,11 +3,45 @@ const inquirer = require('inquirer');
 inquirer
   .prompt([
     {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
+      type: 'list',
+      message: 'Whatt do you want to see?',
+      name: 'selection',
+      choices:[
+        {
+            name: 'First option',
+            value: 'firstOption!'
+        },
+        {
+            name: 'Second option',
+            value: 'secondOption!'
+        }
+      ]
     },
   ])
   .then((response) =>
-    console.log(response.username)
+    {
+    console.log(response.selection);
+    
+    switch(response.selection){
+        case 'firstOption!':
+            firstFunction();
+            break;
+        case 'secondOption!S':
+            secondFunction();
+            break;
+        default:
+            console.log('Default')
+    }
+    
+    
+    }
+    
   );
+
+  var firstFunction= function(){
+      console.log('Fisrt function triggered');
+  }
+
+  var secondFunction=function(){
+      console.log('Second function triggered');
+  }
